23b2059445d8b35cd2bd970c16588818134bbe3b
Melker Öberg <memleoberg@gmail.com>
2023-09-25 23:57:16 +0200

    stable

---
+++ main.js
@@ -0,0 +1,55 @@
+number = []
+
+function button0(){
+    var textfield = document.getElementById("number")
+    textfield.append(0);  
+}
+function button1(){
+    var textfield = document.getElementById("number")
+    textfield.append(1);  
+}
+function button2(){
+    var textfield = document.getElementById("number")
+    textfield.append(2);  
+}
+function button3(){
+    var textfield = document.getElementById("number")
+    textfield.append(3);  
+}
+function button4(){
+    var textfield = document.getElementById("number")
+    textfield.append(4);  
+}
+function button5(){
+    var textfield = document.getElementById("number")
+    textfield.append(5);  
+}
+function button6(){
+    var textfield = document.getElementById("number")
+    textfield.append(6);  
+}
+function button7(){
+    var textfield = document.getElementById("number")
+    textfield.append(7);  
+}
+function button8(){
+    var textfield = document.getElementById("number")
+    textfield.append(8);  
+}
+function button9(){
+    var textfield = document.getElementById("number")
+    textfield.append(9);  
+}
+
+function addFunction(textfieldArray) {
+    var add = document.getElementById("add-btn")
+
+    var textfield = textfield.reduce((a, b) => a + b, 0)
+      
+    console.log(result);
+}
+
+function ce(){
+    var textfield = document.getElementById("number")
+    textfield.innerHTML = null;  
+}
